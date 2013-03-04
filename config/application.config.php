f3f74c732216ae5e99e1c951036c5c99c27359b0
Oleg Lobach <oleg@lobach.info>
2013-03-04 23:27:38 +0400

    добавил новые модули, указал версию ZF 2.1.3

--- config/application.config.php
+++ config/application.config.php
@@ -3,6 +3,12 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
+        'DoctrineModule',
+        'DoctrineMongoODMModule',
+        'ZfcBase',
+        'ZfcUser',
+        'ZfcUserDoctrineMongoODM',
+        'ZendDeveloperTools',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
