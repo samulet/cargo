b5d7180e62f967c8392637b832ffb0cc1cfab28b
salerat <salerat@gmail.com>
2013-04-23 01:50:44 +0700

    Добавлен метод записи в базу организаций.

--- module/Organization/src/Organization/Controller/OrganizationController.php
+++ module/Organization/src/Organization/Controller/OrganizationController.php
@@ -1,37 +1,68 @@
 <?php
-namespace Organization\Controller;
+namespace Organization\Controller {
 
-use Organization\Entity\Company;
-use Zend\Mvc\Controller\AbstractActionController;
-use Zend\View\Model\ViewModel;
-use Organization\Entity\Organization;
+    use Organization\Entity\Company;
+    use Organization\Form\OrganizationCreate;
+    use Zend\Mvc\Controller\AbstractActionController;
+    use Zend\View\Model\ViewModel;
+    use Organization\Entity\Organization;
 
-class OrganizationController extends AbstractActionController
-{
-    public function indexAction()
+    use Doctrine\MongoDB\Connection;
+    use Doctrine\ODM\MongoDB\Configuration;
+    use Doctrine\ODM\MongoDB\DocumentManager;
+    use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
+
+    use Doctrine\ODM\MongoDB\Id\UuidGenerator;
+
+    class OrganizationController extends AbstractActionController
     {
-       error_reporting(E_ALL | E_STRICT) ;
-       ini_set('display_errors', 'On');
-       $objectManager = $this->getServiceLocator()->get('doctrine.documentmanager.odm_default');
+        public function indexAction()
+        {
+            error_reporting(E_ALL | E_STRICT) ;
+            ini_set('display_errors', 'On');
 
-        $org = new Company();
-        $org->setDescription('Test!');
+            $objectManager = $this->getServiceLocator()->get('doctrine.documentmanager.odm_default');
+            $org = $objectManager->getRepository('Organization\Entity\Organization')->findBy(array('activated'=>1));
 
-        $objectManager->persist($org);
-        $objectManager->flush();
+       //     die(var_dump($org));
+            return new ViewModel(array(
+                'org' =>$org
+            ));
+        }
 
-        die(var_dump($org->getId()));
-    }
+        public function addAction()
+        {
+            $form = new OrganizationCreate();
+            return new ViewModel(array(
+                'form' => $form
+            ));
+        }
 
-    public function addAction()
-    {
-    }
+        public function editAction()
+        {
+        }
 
-    public function editAction()
-    {
-    }
+        public function deleteAction()
+        {
+        }
 
-    public function deleteAction()
-    {
+        public function createOrganizationAction(){
+            $var=$this->getRequest()->getPost();
+            if(!empty($var->csrf)) {
+                $objectManager = $this->getServiceLocator()->get('doctrine.documentmanager.odm_default');
+                $org_item=$var->organization;
+                $org = new Organization();
+                $org->setDescription($org_item['description']);
+                $org->setOrgName($org_item['orgName']);
+                $org->setOrgType($org_item['orgType']);
+                $org->setActivated(1);
+                $objectManager->persist($org);
+                $objectManager->flush();
+                return new ViewModel(array(
+                    'result' => 'успешно!'
+                ));
+            }
+            else die();
+        }
     }
 }
