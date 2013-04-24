16488e43c309f0b643d17c18a0bb1d1f2d886125
salerat <salerat@gmail.com>
2013-04-24 22:55:26 +0700

    Добавлен метод просмотра внутрености организации

--- module/Organization/src/Organization/Controller/CompanyController.php
+++ module/Organization/src/Organization/Controller/CompanyController.php
@@ -5,6 +5,7 @@ use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Organization\Entity\Company;
 use Organization\Entity\Organization;
+use Organization\Form\CompanyCreate;
 use Doctrine\ODM\MongoDB\Id\UuidGenerator;
 
 class CompanyController extends AbstractActionController
@@ -16,13 +17,26 @@ class CompanyController extends AbstractActionController
 
     public function addAction()
     {
+        $this->loginControl();
+        $form = new CompanyCreate();
+        return new ViewModel(array(
+            'form' => $form
+        ));
+
+
     }
 
     public function editAction()
     {
+        $org_id=$this->getEvent()->getRouteMatch()->getParam('org_id');
+        die(var_dump($org_id));
     }
 
     public function deleteAction()
     {
+    }
+    private function loginControl() {
+        if ($this->zfcUserAuthentication()->hasIdentity()) return true;
+        else return $this->redirect()->toUrl('/user/login');
     }
 }
