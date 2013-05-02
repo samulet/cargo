d0d7d9d206d1d6acc262aeef0036e1498d85fa0b
salerat <salerat@gmail.com>
2013-05-02 14:47:10 +0700

    Просмотре всех ресурсов, запись и т.д.

--- module/Resource/src/Resource/Controller/ResourceController.php
+++ module/Resource/src/Resource/Controller/ResourceController.php
@@ -9,25 +9,40 @@
 
 namespace Resource\Controller;
 
+    use Entity\Recources;
     use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
-
+    use Zend\Form\Annotation\AnnotationBuilder;
+    use Zend\Form\Element\Checkbox;
     use Zend\Mvc\Controller\AbstractActionController;
     use Zend\View\Model\ViewModel;
 
     class ResourceController extends AbstractActionController
     {
-
+        protected $resourceModel;
 
         public function indexAction()
         {
-
-
+            $res=$this->getResourceModel();
+            return new ViewModel(array(
+                'res' =>  $res->returnAllResource()
+            ));
         }
 
+        public function myAction()
+        {
+            $res=$this->getResourceModel();
+            return new ViewModel(array(
+                'res' => $res->returnMyResource($this->zfcUserAuthentication()->getIdentity()->getId())
+            ));
+        }
 
         public function addAction()
         {
-
+            $builder = new AnnotationBuilder();
+            $form    = $builder->createForm('Resource\Entity\Resource');
+            return new ViewModel(array(
+                'form' => $form
+            ));
         }
 
         public function editAction()
@@ -37,6 +52,12 @@ namespace Resource\Controller;
 
         public function listAction()
         {
+            $id = $this->getEvent()->getRouteMatch()->getParam('id');
+            $res=$this->getResourceModel();
+            //dir(var_dump($res->listResource($id)));
+            return new ViewModel(array(
+                'res' => $res->listResource($id)
+            ));
 
         }
 
@@ -45,6 +66,18 @@ namespace Resource\Controller;
 
          }
 
+        public function addResourceAction() {
+            $res=$this->getResourceModel();
+            $res->addResource($this->getRequest()->getPost(),$this->zfcUserAuthentication()->getIdentity()->getId());
+            return $this->redirect()->toUrl('/resources/my');
+        }
 
-
+        public function getResourceModel()
+        {
+            if (!$this->resourceModel) {
+                $sm = $this->getServiceLocator();
+                $this->resourceModel = $sm->get('Resource\Model\ResourceModel');
+            }
+            return $this->resourceModel;
+        }
 }
