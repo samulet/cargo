f25fd9342ec1c513a94d97641ef5c96064f0f0e8
Oleg Lobach <oleg@lobach.info>
2013-05-17 00:41:47 +0400

    Изменил поведение при редиректе с формы логина/регистрации
    Если залогиненый пользователь перейдет на форму логина или регистрации, то его редиректнет снова на форму логина и так по кругу.
    Для борьбы с таким поведением я реализовал собственную стратегию, где добавил проверку на роуты логина и регистрации. В этом случае редирект будет на страницу профиля пользователя.

--- module/User/config/module.config.php
+++ module/User/config/module.config.php
@@ -5,4 +5,9 @@ return array(
             'user' => __DIR__ . '/../view',
         ),
     ),
+    'service_manager' => array(
+        'invokables'  => array(
+            'BjyAuthorize\View\RedirectionStrategy' => 'User\View\RedirectionStrategy',
+        ),
+    ),
 );
