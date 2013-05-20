e528947e55c2e78099ed376d05f17c2ad636fafd
salerat <salerat@gmail.com>
2013-05-20 18:33:37 +0700

    Сделал базу для списков

---
+++ module/AddList/src/AddList/Entity/AddList.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: solov
+ * Date: 5/20/13
+ * Time: 2:31 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace AddList\Entity;
+
+use Zend\Form\Annotation;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use Zend\Form\Element;
+use Zend\Form\Form;
+use Doctrine\ODM\MongoDB\Id\UuidGenerator;
+use Doctrine\ODM\MongoDB\Mapping\Types\Type;
+
+
+/**
+ * @ODM\Document(collection="addList")
+ * @Annotation\Name("addList")
+ * @Annotation\Hydrator("Zend\Stdlib\Hydrator\ObjectProperty")
+ */
+class AddList
+{
+
+    /**
+     * @ODM\Id
+     * @var int
+     * @Annotation\Exclude()
+     */
+    public $id;
+
+    /**
+     * @Gedmo\Timestampable(on="create")
+     * @ODM\Date
+     * @Annotation\Exclude()
+     */
+    public $created;
+    /**
+     * @Gedmo\Timestampable(on="update")
+     * @ODM\Date
+     * @Annotation\Exclude()
+     */
+    public $updated;
+    /**
+     * @var string
+     * @ODM\Field(type="string")
+     * @Annotation\Type("Zend\Form\Element\Select")
+     * @Annotation\Required({"required":"true" })
+     * @Annotation\Filter({"name":"StripTags"})
+     * @Annotation\Options({"label":"Лист:",
+     *                      "value_options" : {"ticket-cargo":"Заявка - тип грузка","vehicle-mark":"ТС - марка","vehicle-model":"ТС - модель","vehicle-type":"ТС - тип"}})
+     * @Annotation\Validator({"name":"InArray",
+     *                        "options":{"haystack":{"1","2","3"},
+     *                              "messages":{"notInArray":"Please Select a Class"}}})
+     * @Annotation\Attributes({"value":"0"})
+     */
+    public $listName;
+
+    /**
+     * @Annotation\Filter({"name":"StringTrim"})
+     * @Annotation\Validator({"name":"StringLength", "options":{"min":1, "max":25}})
+     * @Annotation\Validator({"name":"Regex", "options":{"pattern":"/^[a-zA-Z][a-zA-Z0-9_-]{0,24}$/"}})
+     * @Annotation\Attributes({"type":"text"})
+     * @Annotation\Options({"label":"Занчение в БД"})
+     * @Annotation\Required({"required":"true" })
+     * @var string
+     * @ODM\Field(type="string")
+     */
+    public $key;
+
+    /**
+     * @Annotation\Filter({"name":"StringTrim"})
+     * @Annotation\Validator({"name":"StringLength", "options":{"min":1, "max":25}})
+     * @Annotation\Validator({"name":"Regex", "options":{"pattern":"/^[a-zA-Z][a-zA-Z0-9_-]{0,24}$/"}})
+     * @Annotation\Attributes({"type":"text"})
+     * @Annotation\Options({"label":"Имя поля"})
+     * @Annotation\Required({"required":"true" })
+     * @var string
+     * @ODM\Field(type="string")
+     */
+    public $value;
+    /**
+     * @Annotation\Type("Zend\Form\Element\Submit")
+     * @Annotation\Attributes({"value":"Отправить"})
+     */
+    public $submit;
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+}
--- module/AddList/src/AddList/View/AddList.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: solov
- * Date: 5/20/13
- * Time: 2:31 AM
- * To change this template use File | Settings | File Templates.
- */
