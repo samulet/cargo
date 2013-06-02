8b87746e311407304784f2a55164e72660160c38
salerat <salerat@gmail.com>
2013-06-03 02:17:24 +0700

    Общая заготовка модуля взаимодействия.

---
+++ module/Interaction/Module.php
@@ -0,0 +1,45 @@
+<?php
+namespace Interaction;
+
+
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+use Interaction\Model\InteractionModel;
+
+
+class Module
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+
+        return array(
+            'factories' => array(
+                'Interaction\Model\InteractionModel' => function ($sm) {
+                    $auc = new InteractionModel();
+                    return $auc;
+                },
+            ),
+        );
+    }
+
+}
---
+++ module/Interaction/config/module.config.php
@@ -0,0 +1,53 @@
+<?php
+namespace Interaction;
+
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Interaction\Controller\Interaction' => 'Interaction\Controller\InteractionController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'interaction' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/interactions[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id' => '[a-z0-9]*',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Interaction\Controller\Interaction',
+                        'action' => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'bjyauthorize' => array(
+        'guards' => array(
+            'BjyAuthorize\Guard\Route' => array(
+                array('route' => 'interaction', 'roles' => array('user')),
+            ),
+        ),
+    ),
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver',
+                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+            ),
+            'odm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                )
+            )
+        )
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'interaction' => __DIR__ . '/../view',
+        ),
+    ),
+);
