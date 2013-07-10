97fc0048ed0bfef12f5727674648ad2c261bf8b7
salerat <salerat@gmail.com>
2013-07-10 11:17:13 +0700

    убрал предыдущий патч)

--- module/Ticket/src/Ticket/Entity/TicketWay.php
+++ module/Ticket/src/Ticket/Entity/TicketWay.php
@@ -374,7 +374,7 @@ class TicketWay
      * @Annotation\Type("Zend\Form\Element\Select")
 
      * @Annotation\Filter({"name":"StripTags"})
-     * @Annotation\Options({"label":"Тип загрузки"})
+     * @Annotation\Options({"label":"Тип выгрузки"})
      * @Annotation\Validator({"name":"InArray",
      *                        "options":{"haystack":{"1","2","3"},
      *                              "messages":{"notInArray":"Please Select a Class"}}})
