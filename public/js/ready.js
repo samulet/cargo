d749f988d62e44c2b0565b88ef106c4844dddc69
salerat <salerat@gmail.com>
2013-07-15 16:51:17 +0700

    сделал скрипт для правильного введения даты

--- public/js/ready.js
+++ public/js/ready.js
@@ -47,3 +47,72 @@ $(document).ready(function() {
     });
 
 });
+
+function time_control(element) {
+    var numCount=$(element).val().length;
+    var numVal=$(element).val();
+    var lenCounter=numCount;
+    if(numCount==2) {
+        if(numVal.substring(1,2)==':') {
+            numVal=new String('0'+numVal);
+            $(element).val( numVal );
+            numCount=3;
+            lenCounter=numCount
+        }
+    }
+    var regEx1=/[0-2]/;
+    var regEx24=/[0-9]/;
+    var regEx3=/[0-5]/;
+    var error=0;
+    if(lenCounter>5) {
+        numVal=numVal.substring(0,5);
+        numCount=5;
+        lenCounter=5;
+        $(element).val( numVal );
+    }
+    if(lenCounter==5) {
+        if(regEx24.exec( numVal.substring(4,5) )==null) {
+            numVal=numVal.substring(0,4);
+            error++;
+        }
+        lenCounter--;
+
+    }
+
+    if(lenCounter==4) {
+        if(regEx3.exec( numVal.substring(3,4) )==null) {
+            numVal=numVal.substring(0,3);
+            error++;
+        }
+        lenCounter--;
+    }
+
+    if(lenCounter==3) {
+        if(numVal.substring(2,3)!=':') {
+            numVal=numVal.substring(0,2);
+            error++;
+        }
+
+        lenCounter--;
+    }
+
+    if(lenCounter==2) {
+        if(regEx3.exec( numVal.substring(1,2) )==null) {
+            numVal=numVal.substring(0,1);
+            error++;
+        }
+
+        lenCounter--;
+    }
+    if(lenCounter==1) {
+        if(regEx1.exec( numVal.substring(0,1) )==null) {
+            numVal='';
+            error++;
+        }
+
+        lenCounter--;
+    }
+    if(error>0) {
+        $(element).val( numVal );
+    }
+}
