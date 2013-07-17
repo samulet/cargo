cfd5aacd3e9a267e20f5d6fa9e9e6794ff6fde04
salerat <salerat@gmail.com>
2013-07-17 16:56:31 +0700

    таблица данных дял множесвенных документов

---
+++ module/Ticket/src/Ticket/Entity/DocumentWay.php
@@ -0,0 +1,354 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: solov
+ * Date: 7/17/13
+ * Time: 4:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Ticket\Entity;
+
+use Zend\Form\Annotation;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use Zend\Form\Element;
+use Zend\Form\Form;
+use Doctrine\ODM\MongoDB\Id\UuidGenerator;
+
+
+/**
+ * @ODM\Document(collection="documentWay")
+ * @Gedmo\SoftDeleteable(fieldName="deletedAt")
+ * @Annotation\Name("documentWay")
+ * @Annotation\Hydrator("Zend\Stdlib\Hydrator\ObjectProperty")
+ */
+class DocumentWay
+{
+    public function __construct()
+    {
+        $uuid_gen = new UuidGenerator();
+        $this->setUUID($uuid_gen->generateV4());
+    }
+
+    /**
+     * @ODM\Id
+     * @var int
+     * @Annotation\Exclude()
+     */
+    public $id;
+    /**
+     * @var string
+     * @ODM\Field(type="string")
+     * @Annotation\Exclude()
+     */
+    public $uuid;
+    /**
+     * @ODM\ObjectId
+     * @var int
+     * @Annotation\Exclude()
+     */
+    public $ownerTicketWayId;
+
+    /**
+     * @Gedmo\Timestampable(on="create")
+     * @ODM\Date
+     * @Annotation\Exclude()
+     */
+    public $created;
+    /**
+     * @Gedmo\Timestampable(on="update")
+     * @ODM\Date
+     * @Annotation\Exclude()
+     */
+    public $updated;
+    /**
+     * @var string
+     * @ODM\Field(type="string")
+     * @Annotation\Exclude()
+     */
+    public $activated;
+
+
+    /**
+     * @Annotation\Type("Zend\Form\Element\Submit")
+     * @Annotation\Attributes({"value":"Отправить"})
+     */
+    public $submit;
+
+
+    /**
+     * @Annotation\Filter({"name":"StringTrim"})
+     * @Annotation\Validator({"name":"StringLength", "options":{"min":1, "max":25}})
+     * @Annotation\Validator({"name":"Regex", "options":{"pattern":"/^[A-Za-zА-Яа-я0-9 _-]*$/iu"}})
+     * @Annotation\Attributes({"type":"text"})
+     * @Annotation\Options({"label":"Номер документа"})
+     * @Annotation\Required({"required":"true" })
+     * @var string
+     * @ODM\Field(type="string")
+     */
+    public $docNumber;
+
+    /**
+     * @var string
+     * @ODM\Field(type="string")
+     * @Annotation\Type("Zend\Form\Element\Select")
+
+     * @Annotation\Filter({"name":"StripTags"})
+     * @Annotation\Options({"label":"Вид документа"})
+     * @Annotation\Required({"required":"true" })
+     * @Annotation\Attributes({"value":"0"})
+     */
+    public $docType;
+    /**
+     * @var string
+     * @ODM\Field(type="string")
+     * @Annotation\Type("Zend\Form\Element\Date")
+     * @Annotation\Required({"required":"true" })
+     * @Annotation\Options({"label":"Дата докумениа"})
+     */
+
+    public $docDate;
+
+
+    /**
+     * @Annotation\Filter({"name":"StringTrim"})
+     * @Annotation\Validator({"name":"StringLength", "options":{"min":1, "max":500}})
+     * @Annotation\Validator({"name":"Regex", "options":{"pattern":"/^[A-Za-zА-Яа-я0-9 _-]*$/iu"}})
+     * @Annotation\Type("Zend\Form\Element\Textarea")
+     * @Annotation\Options({"label":"Комментарий документ"})
+     * @var string
+     * @ODM\Field(type="string")
+     */
+    public $docNote;
+    /**
+     * @var string
+     * @ODM\Field(type="string")
+     * @Annotation\Type("Zend\Form\Element\Select")
+
+     * @Annotation\Filter({"name":"StripTags"})
+     * @Annotation\Options({"label":"Возвратный документ","value_options":{"":"Выберите значение","yes":"да", "no":"нет"}})
+     * @Annotation\Validator({"name":"InArray",
+     *                        "options":{"haystack":{"yes","no","3"},
+     *                              "messages":{"notInArray":"Please Select a Class"}}})
+     * @Annotation\Attributes({"value":"0"})
+     */
+    public $docWay;
+
+    /**
+     * @ODM\Date
+     * @Annotation\Exclude()
+     */
+    public $deletedAt;
+    /**
+     * @return mixed
+     */
+    public function getDeletedAt()
+    {
+        return $this->deletedAt;
+    }
+
+    /**
+     * @param mixed $deletedAt
+     */
+    public function setDeletedAt($deletedAt)
+    {
+        $this->deletedAt = $deletedAt;
+    }
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * Get description.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set Description.
+     *
+     * @param string $description
+     * @return UserInterface
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Get activated.
+     *
+     * @return string activated
+     */
+    public function getActivated()
+    {
+        return $this->activated;
+    }
+
+    /**
+     * Set activated.
+     *
+     * @param string $activated
+     * @return UserInterface
+     */
+    public function setActivated($activated)
+    {
+        $this->activated = $activated;
+        return $this;
+    }
+
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+    }
+
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    public function getUUID()
+    {
+        return $this->uuid;
+    }
+
+    public function setUUID($uuid)
+    {
+        $this->uuid = $uuid;
+        return $this;
+    }
+
+    public function getSizes()
+    {
+        return $this->sizes;
+    }
+
+    public function setCapacity($capacity)
+    {
+        $this->capacity = $capacity;
+        return $this;
+    }
+
+    public function getCapacity()
+    {
+        return $this->capacity;
+    }
+
+    public function setAddressFact($addressFact)
+    {
+        $this->addressFact = $addressFact;
+        return $this;
+    }
+
+    public function getAddressReg()
+    {
+        return $this->addressReg;
+    }
+
+    public function setAddressReg($addressReg)
+    {
+        $this->addressReg = $addressReg;
+        return $this;
+    }
+
+    public function getGeneralManager()
+    {
+        return $this->generalManager;
+    }
+
+    public function setGeneralManager($generalManager)
+    {
+        $this->generalManager = $generalManager;
+        return $this;
+    }
+
+    public function getTelephone()
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone($telephone)
+    {
+        $this->telephone = $telephone;
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getOwnerOrgId()
+    {
+        return $this->ownerOrgId;
+    }
+
+    public function setOwnerOrgId($ownerOrgId)
+    {
+        $this->ownerOrgId = $ownerOrgId;
+        return $this;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set type.
+     *
+     * @param string $type
+     * @return OrganizationInterface
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getTent()
+    {
+        return $this->name;
+    }
+
+    public function setTent($tent)
+    {
+        $this->tent = $tent;
+        return $this;
+    }
+}
