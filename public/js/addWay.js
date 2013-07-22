1e77a9376921da63882b82cc8dfa6e6999fd91c7
salerat <salerat@gmail.com>
2013-07-22 13:13:16 +0700

    полностью поправил добавление множественных документов

--- public/js/addWay.js
+++ public/js/addWay.js
@@ -7,6 +7,8 @@ function addWay(wayTr) {
 
     $('<tr class=' + wayTr + '-' + counterVal + '>' + way.html() + '</tr>').insertBefore('.addTr');
 
+
+
     $('.' + wayTr + '-' + counterVal + ' input').each(function (index) {
         if (typeof $(this).attr('name') === 'undefined') {
         } else {
@@ -21,14 +23,18 @@ function addWay(wayTr) {
             if ( (splitEl2==0) || (typeof splitEl[1] === 'undefined') ){
 
                 $(this).attr('name', $(this).attr('name') + '-' + counterVal);
-                if (($(this).attr('class') != 'deleteWay') && ( $(this).attr('class') != 'deleteDocWay' ) && ( $(this).attr('class') != 'addDocWay' )) {
+                if (($(this).attr('class') != 'deleteWay') && ( $(this).attr('class') != 'deleteDocWay' ) && ( $(this).attr('class') != 'addDocWay' ) && ($(this).attr('class') != 'docCounter') ) {
                     $(this).val('');
                 } else {
-                    alert($(this).attr('class'));
                     if( ( $(this).attr('class') == 'addDocWay' )) {
                         $(this).attr('onclick', "addDocWay('"+ wayTr + '-' + counterVal +"','docBox');");
                     }
+                    if( ( $(this).attr('class') == 'docCounter' )) {
+                        $(this).val(1);
+                    }
+
                 }
+
             } else {
                 if(splitEl2 % 1 === 0) {
                     $(this).parent().parent().remove();
@@ -39,22 +45,42 @@ function addWay(wayTr) {
     $('.' + wayTr + '-' + counterVal + ' select').each(function (index) {
         if (typeof $(this).attr('name') === 'undefined') {
         } else {
-            var splitEl = $(this).attr('name').split('_')[1];
-            var splitEl2 = splitEl.split('-');
-            if (typeof splitEl2[1] === 'undefined') {
+            var splitEl = $(this).attr('name').split('_');
+            var splitEl2='none';
+            if (typeof splitEl[1] === 'undefined') {
+            } else {
+
+                splitEl2=splitEl[1];
+
+            }
+            if ( (splitEl2==0) || (typeof splitEl[1] === 'undefined') ){
                 $(this).attr('name', $(this).attr('name') + '-' + counterVal);
                 $(this).val('');
+            } else {
+                if(splitEl2 % 1 === 0) {
+                    $(this).parent().parent().remove();
+                }
             }
         }
     });
     $('.' + wayTr + '-' + counterVal + ' textarea').each(function (index) {
         if (typeof $(this).attr('name') === 'undefined') {
         } else {
-            var splitEl = $(this).attr('name').split('_')[1];
-            var splitEl2 = splitEl.split('-');
-            if (typeof splitEl2[1] === 'undefined') {
+            var splitEl = $(this).attr('name').split('_');
+            var splitEl2='none';
+            if (typeof splitEl[1] === 'undefined') {
+            } else {
+
+                splitEl2=splitEl[1];
+
+            }
+            if ( (splitEl2==0) || (typeof splitEl[1] === 'undefined') ){
                 $(this).attr('name', $(this).attr('name') + '-' + counterVal);
                 $(this).val('');
+            } else {
+                if(splitEl2 % 1 === 0) {
+                    $(this).parent().parent().remove();
+                }
             }
         }
     });
@@ -75,9 +101,11 @@ function addDocWay(wayTr, docBox) {
     var wayDoc = $('.' + wayTr + ' .' + docBox);
     var counter = $('.' + wayTr + ' .docCounter');
     var counterVal = counter.val();
-    $('<div class=' + docBox + '_' + counterVal + '>' + wayDoc.html() + '<div>').insertBefore('.addDocBefore');
+    $('<div class=' + docBox + '_' + counterVal + '>' + wayDoc.html() + '<div>').insertBefore('.' + wayTr +' .addDocBefore')
 
-    $('.' + wayTr + ' .' + docBox + '_' + counterVal + ' input').each(function (index) {
+    $('.' + wayTr + ' .' + docBox + '_' + counterVal + ' .docName').html('<b>Документ '+counterVal+'</b>');
+
+    $('.' + wayTr + ' .' + docBox + '_' + counterVal + ' input'+','+'.' + wayTr + ' .' + docBox + '_' + counterVal + ' select'+', '+'.' + wayTr + ' .' + docBox + '_' + counterVal + ' textarea').each(function (index) {
         if (typeof $(this).attr('name') === 'undefined') {
         } else {
             var splitName = $(this).attr('name').split('_');
@@ -89,7 +117,7 @@ function addDocWay(wayTr, docBox) {
             var splitCounterLast = '';
             if (typeof splitCounter[1] === 'undefined') {
             } else {
-                splitCounterLast = '-' + splitCounter;
+                splitCounterLast = '-' + splitCounter[1];
             }
             $(this).attr('name', splitName + '_' + counterVal + splitCounterLast);
             if ($(this).attr('class') != 'deleteDocWay') {
@@ -97,15 +125,7 @@ function addDocWay(wayTr, docBox) {
             }
         }
     });
-    $('.' + wayTr + ' .' + docBox + '_' + counterVal + ' select').each(function (index) {
-        $(this).attr('name', $(this).attr('name') + '-' + counterVal);
-        $(this).val('');
-    });
-    $('.' + wayTr + ' .' + docBox + '_' + counterVal + ' textarea').each(function (index) {
 
-        $(this).attr('name', $(this).attr('name') + '-' + counterVal);
-        $(this).val('');
-    });
 
     counterVal++;
     $('.' + wayTr + ' .docCounter').val(counterVal);
