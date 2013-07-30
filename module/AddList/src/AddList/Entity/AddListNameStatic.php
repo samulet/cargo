1281b893aaea5f61b4a45782a87d4e01d1846bcb
salerat <salerat@gmail.com>
2013-07-30 20:35:29 +0700

    сделал класс, с статическим списком

---
+++ module/AddList/src/AddList/Entity/AddListNameStatic.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: solov
+ * Date: 7/30/13
+ * Time: 7:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class AddListNameStatic
+{
+    public static $list = array(
+        "veh-marks" => "Марки транспортных средств",
+        "veh-models" => "Модели транспортных средств",
+        "veh-status" => "Статусы транспортных средств",
+        "veh-type" => "Типы транспортных средств",
+        "ownerships" => "Формы собственности компаний" ,
+        "requisites" => "Реквизиты компаний",
+        "prod-group" => "Продуктовая группа груза",
+        "doc-type" => "Виды документов заявки",
+        "load-type" => "Виды загрузки",
+        "offer-status" => "Статусы предложений",
+        "temp-cond" => "Температурные режимы",
+        "country" => "Страны",
+        "regions" => "Регионы",
+        "city" => "Города" 	,
+        "address" => "Адреса"
+    );
+
+}
