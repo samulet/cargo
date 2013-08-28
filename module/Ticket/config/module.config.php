0c029c9a3766006234902f2e6d4f61f9785a2088
salerat <salerat@gmail.com>
2013-08-28 15:54:51 +0700

    поправил поля у заявки

--- module/Ticket/config/module.config.php
+++ module/Ticket/config/module.config.php
@@ -44,9 +44,10 @@ return array(
     'bjyauthorize' => array(
         'guards' => array(
             'BjyAuthorize\Guard\Controller' => array(
-                array('controller' => 'Ticket\Controller\Ticket','action'=> array('index','search','add','delete', 'addTicket', 'getResults','copy','getExcel','getExcel2'),'roles' => array('admin')),
-                array('controller' => 'Ticket\Controller\Ticket','action'=> array('index','add','search', 'getResults','getExcel'),'roles' => array('forwarder','carrier','admin')),
-                array('controller' => 'Ticket\Controller\Ticket','action'=> array('my','add','edit','delete','copy','list','getExcel'),'roles' => array('forwarder','customer')),
+                array('controller' => 'Ticket\Controller\Ticket','action'=> array('add'),'roles' => array('forwarder','carrier','customer','admin')),
+                array('controller' => 'Ticket\Controller\Ticket','action'=> array('my','delete'),'roles' => array('forwarder','customer','admin')),
+                array('controller' => 'Ticket\Controller\Ticket','action'=> array('index','search', 'getResults'),'roles' => array('forwarder','carrier','admin')),
+
 
                 array('controller' => 'Ticket\Controller\Cargo', 'action' => array('index'), 'roles' => array('admin')),
                 array('controller' => 'Ticket\Controller\Cargo', 'action' => array('my','add', 'edit', 'list', 'delete', 'addCargo', 'copy'), 'roles' => array('forwarder','customer','admin')),
