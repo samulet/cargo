190c00da5fc546d8c4f2b69fc32c19649e4b2ae2
salerat <salerat@gmail.com>
2013-09-05 00:49:23 +0700

    статический список фильтров

---
+++ module/Ticket/src/Ticket/Entity/FiltersArrayStatic.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: salerat
+ * Date: 9/5/13
+ * Time: 12:43 AM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Ticket\Entity;
+class FiltersArrayStatic
+{
+    public static $list = array(
+        'dateStartFilterFrom'=>'',
+        'dateStartFilterTo'=>'',
+        'createdFilterFrom'=>'',
+        'createdFilterTo'=>'',
+        'ownerId'=>''
+    );
+
+}
