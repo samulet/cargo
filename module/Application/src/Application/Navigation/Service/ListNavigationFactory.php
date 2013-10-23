191aef3732dba650b3b09ba9f9a09622a3471638
salerat <salerat@gmail.com>
2013-10-23 23:19:34 +0700

    научился вытаскивать юрл в фабрике

--- module/Application/src/Application/Navigation/Service/ListNavigationFactory.php
+++ module/Application/src/Application/Navigation/Service/ListNavigationFactory.php
@@ -2,7 +2,8 @@
 namespace Application\Navigation\Service;
 
 use Zend\Navigation\Service\DefaultNavigationFactory;
-
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\Http\Exception;
 class ListNavigationFactory extends DefaultNavigationFactory
 {
     /**
@@ -11,5 +12,13 @@ class ListNavigationFactory extends DefaultNavigationFactory
     protected function getName()
     {
         return 'list';
+    }
+
+    protected function getPages(ServiceLocatorInterface $serviceLocator)
+    {
+
+        $vhm = $serviceLocator->get('viewhelpermanager');
+        $url = $vhm->get('url');
+
     }
 }
