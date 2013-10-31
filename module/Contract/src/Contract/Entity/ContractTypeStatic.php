205526a0cd590299828c33bc3a38d8f6356fcb55
salerat <salerat@gmail.com>
2013-10-31 22:09:14 +0700

    CARGO-89 добавил первую версию статических типов

---
+++ module/Contract/src/Contract/Entity/ContractTypeStatic.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: salerat
+ * Date: 10/30/13
+ * Time: 11:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Contract\Entity;
+
+class ContractTypeStatic {
+    public static $type = array(
+        'contractEcs' => array(
+            'rusName' => 'Договор экспедиции'
+        ),
+        'contractTr'=> array(
+            'rusName' => 'Договор перевозки'
+        ),
+    );
+}
