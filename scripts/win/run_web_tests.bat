fe15a3b80690565e1be11348cf5b9864d37c6d7b
Light <light@lionhearth.com>
2013-11-08 13:55:10 +0900

    Добавлен скрипт запуска ассептанс веб-тестов

---
+++ scripts/win/run_web_tests.bat
@@ -0,0 +1,3 @@
+#should runs from root folder (please use mingw or cygwin console)
+#running web-tests based on Chromedriver and Stand-Alone Selenium server
+./node_modules/protractor/bin/protractor ./node_modules/protractor/referenceConf.js
