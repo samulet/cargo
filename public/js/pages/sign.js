29ddeea5d57d1b0fa965151e0518ccb1dc47f1e8
S.Panfilov <shade241241@gmail.com>
2013-11-11 17:28:37 +0900

    Отлажена форма логина на главной странице

--- public/js/pages/sign.js
+++ public/js/pages/sign.js
@@ -5,17 +5,18 @@ angular.module('website.sign', [])
     .controller('signUpController', ['$scope', '$http', 'storageFactory', function ($scope, $http, storageFactory) {
         //
     }])
-    .controller('signInController', ['$scope', '$http', 'storageFactory', function ($scope, $http, storageFactory) {
+    .controller('signInController', ['$scope', '$rootScope', '$http', 'storageFactory', function ($scope, $rootScope, $http, storageFactory) {
+        $rootScope.pageTitle = 'Вход';
+
         function onError(data, error) {
             //
         }
 
-        $scope.signUp = function () {
+        $scope.signIn = function () {
             $http.post('', {
-                email: this.signUpData.email,
-                password: this.signUpData.password
+                email: this.signInData.email,
+                password: this.signInData.password
             }).success(function (data) {
-                    alert('success sign up');
                     storageFactory.setUser(data.user);
                 }).error(onError);
         };
