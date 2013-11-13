8b19461f6379950e8e46b2471768c6a3f9bdf0f0
S.Panfilov <se-panfilov@ya.ru>
2013-11-13 17:23:19 +0900

    Подготовлен html шаблон профиля пользователя для взаимодействия с основным js

--- public/js/compiled/cargo.js
+++ public/js/compiled/cargo.js
@@ -291,6 +291,15 @@ angular.module('website.user.profile', [])
 
     .controller('userProfileController', ['$scope', '$rootScope', '$http', 'storageFactory', 'errorFactory', 'redirectFactory', function ($scope, $rootScope, $http, storageFactory, errorFactory, redirectFactory) {
         $rootScope.pageTitle = 'Профиль';
+        $scope.editMode = false;
+
+        $scope.profileData = {
+            fio: "-",
+            email: "-",
+            passportData: "-",
+            phones: "-"
+        }
+
 
     }])
 ;
