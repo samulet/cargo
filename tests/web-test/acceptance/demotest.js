65c9ef1a002d145a558df3659deecc5cce29c16e
S.Panfilov <se-panfilov@ya.ru>
2013-11-19 18:00:15 +0900

    Добавлены загодтовки под acceptance тесты веб части

--- tests/web-test/acceptance/demotest.js
+++ tests/web-test/acceptance/demotest.js
@@ -1,11 +1,62 @@
-describe('angularjs homepage', function() {
-    it('should greet the named user', function() {
-        browser.get('http://www.angularjs.org');
+describe('user profile page', function () {
+    var ptor = protractor.getInstance();
 
-        element(by.model('yourName')).sendKeys('Julie');
-
-        var greeting = element(by.binding('yourName'));
-
-        expect(greeting.getText()).toEqual('Hello Julie!');
+    beforeEach(function () {
+        ptor.get('#!/user/profile');
+        /*button = ptor.findElement(protractor.By.className('btn-say-hello'));
+         button.click();*/
     });
+
+    function getButton(name) {
+        return ptor.findElement(protractor.By.xpath('//button[contains(.,"' + name + '")]'));
+    }
+
+    function getHeader(level, text) {
+        return ptor.findElement(protractor.By.xpath('//h' + level + '[contains(.,"' + text + '")]'));
+    }
+
+    it('checkDefaultPageView', function () {
+        var editBtn = getButton('Редактировать');
+        expect(editBtn.getAttribute('disabled'));
+        //getHeader(4, 'Личные данные').isDisplayed();
+        //getHeader(4, 'Личные данные');
+        getHeader(4, 'Паспортные sadsd данные')();
+        getHeader(4, 'Прочие данные');
+        getHeader(4, 'Телефоны');
+        getHeader(4, 'Почтовые адреса');
+        getHeader(4, 'Email');
+        getHeader(4, 'Сайты');
+        getHeader(4, 'Фотография профиля');
+        getHeader(4, 'Цифровая подпись');
+        getHeader(4, 'Аккаунты соцальных сетей');
+
+    });
+
+    it('checkEditMode', function () {
+        var editBtn = getButton('Редактировать');
+        editBtn.click();
+        expect(editBtn.getAttribute('disabled'));
+        getHeader(4, 'Личные данные');
+        getHeader(4, 'Паспортные данные');
+        getHeader(4, 'Прочие данные');
+        getHeader(4, 'Телефоны');
+        getHeader(4, 'Почтовые адреса');
+        getHeader(4, 'Email');
+        getHeader(4, 'Сайты');
+        getHeader(4, 'Фотография профиля');
+        getHeader(4, 'Цифровая подпись');
+        getHeader(4, 'Аккаунты соцальных сетей');
+
+    });
+
+    /*it('checkPhoneBlockDefaultState', function() {
+     message = ptor.findElement(protractor.By.name('phonesDataForm'));
+     expect(message.getText()).toEqual('Hello!');
+     });*/
+
+    /*it('checkPhoneBlockVisibleSwitch', function() {
+     var form = ptor.findElement(protractor.By.name('phonesDataForm'));
+     form.findElement(protractor.By.name("showAddPhoneForm")).click();
+     //expect(message.getText()).toEqual('Hello!');
+     });*/
 });
