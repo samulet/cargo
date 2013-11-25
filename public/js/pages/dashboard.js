fb9618a9950bf5f0c396381e4269ba34183fb814
S.Panfilov <se-panfilov@ya.ru>
2013-11-25 18:05:15 +0900

    Добавлен шаблон для заполнения даных юридического лица

--- public/js/pages/dashboard.js
+++ public/js/pages/dashboard.js
@@ -2,7 +2,26 @@
 
 angular.module('website.dashboard', [])
 
-    .controller('dashboardController', ['$scope', '$rootScope', function ($scope, $rootScope) {
-        $rootScope.pageTitle = 'dash';
+    .controller('dashboardController', ['$scope', '$rootScope', '$http', 'REST_CONFIG', 'errorFactory', 'RESPONSE_STATUS', function ($scope, $rootScope, $http, REST_CONFIG, errorFactory, RESPONSE_STATUS) {
+        $rootScope.pageTitle = 'dashboard';
+        $rootScope.bodyColor = 'filled_bg';
+        $scope.firstTimeVisit = true; //TODO false
+
+        function onError(data, status) {
+            if (status === RESPONSE_STATUS.NOT_FOUND) {
+                $scope.firstTimeVisit = true;
+            } else {
+                errorFactory.resolve(data, status, true);
+            }
+        }
+
+        $scope.getAccounts = function () {
+            $http.get(REST_CONFIG.BASE_URL + '/accounts').
+                success(function (data) {
+
+                }).error(onError);
+        };
+
+
     }])
 ;
