c542fdc13a6d303f3d74cde4665fabc44e04c666
S.Panfilov <se-panfilov@ya.ru>
2013-11-26 15:14:08 +0900

    Поправлен контроллер для страницы дашборда - отлажена проверка наличия аккаунтов и отображения модального окна

--- public/js/compiled/cargo.js
+++ public/js/compiled/cargo.js
@@ -276,18 +276,11 @@ angular.module('website.dashboard', [])
     .controller('dashboardController', ['$scope', '$rootScope', '$http', 'REST_CONFIG', 'errorFactory', 'RESPONSE_STATUS', 'storageFactory', '$modal', function ($scope, $rootScope, $http, REST_CONFIG, errorFactory, RESPONSE_STATUS, storageFactory, $modal) {
         $rootScope.pageTitle = 'dashboard';
         $rootScope.bodyColor = 'filled_bg';
-        $scope.firstTimeVisit = false;
 
-        $scope.showPopup = function (){
-            $modal.open({
-                templateUrl: 'accountModalContent.html',
-                controller: 'accountModalController'
-            });
-        };
+        getAccounts();
 
         function onError(data, status) {
             if (status === RESPONSE_STATUS.NOT_FOUND) {
-                $scope.firstTimeVisit = true;
                 $modal.open({
                     templateUrl: 'accountModalContent.html',
                     controller: 'accountModalController'
@@ -297,14 +290,14 @@ angular.module('website.dashboard', [])
             }
         }
 
-        $scope.getAccounts = function () {
+        function getAccounts() {
             if (!storageFactory.getAccounts()) {
-                $http.get(REST_CONFIG.BASE_URL + '/accounts').
-                    success(function (accounts) {
+                $http.get(REST_CONFIG.BASE_URL + '/accounts')
+                    .success(function (accounts) {
                         storageFactory.setAccounts(accounts);
                     }).error(onError);
             }
-        };
+        }
     }])
 
     .controller('accountModalController', ['$scope', '$http', 'REST_CONFIG', 'errorFactory', 'RESPONSE_STATUS', 'storageFactory', function ($scope, $http, REST_CONFIG, errorFactory, RESPONSE_STATUS, storageFactory) {
