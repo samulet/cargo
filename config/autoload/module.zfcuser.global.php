d223bad327df3416e7f7fa5dbdaff4ba33757b93
Oleg Lobach <oleg@lobach.info>
2013-12-06 11:26:54 +0400

    Изменил значение статуса пользователя по умолчнанию при регистрации
    Нулевое значение статуса не попадало в базу, что приводило к ошибкам в дальнейшей работе

--- config/autoload/module.zfcuser.global.php
+++ config/autoload/module.zfcuser.global.php
@@ -203,7 +203,7 @@ $settings = array(
      * What state user should have upon registration?
      * Allowed value type: integer
      */
-    'default_user_state' => 0,
+    'default_user_state' => 1,
 
     /**
      * States which are allowing user to login
@@ -212,7 +212,7 @@ $settings = array(
      * Include null if you want user's with no state to login as well.
      * Allowed value types: null and integer
      */
-    'allowed_login_states' => array( null, 0, 1, 2 ),
+    'allowed_login_states' => array( 1, 2, 3, 4 ),
 
     /**
      * User table name
