17319092d26e9601fb36688813446f005c262551
S.Panfilov <shade241241@gmail.com>
2013-12-13 13:44:00 +0900

    Исправлена работа директив меню пользователя, для выбора юр. лица и аккаунта

--- public/js/partials/top_menu.js
+++ public/js/partials/top_menu.js
@@ -6,7 +6,18 @@ angular.module('website.top.menu', [])
         return {
             restrict: 'E',
             templateUrl: 'html/partials/private/top_menu.html',
-            controller: function ($scope, $location) {
+            controller: function ($scope, $location, storageFactory) {
+                var user = storageFactory.getUser();
+                var selectedAccount = storageFactory.getSelectedAccount();
+                var selectedCompany = storageFactory.getSelectedCompany();
+
+                $scope.accountShowModal = false;
+                $scope.companyShowModal = false;
+
+                $scope.displayName = (user) ? user.name : 'Пользователь';
+                $scope.accountName = (selectedAccount) ? selectedAccount.title : '(Нет аккаунта)';
+                $scope.companyShortName = (selectedCompany) ? selectedCompany.short : '(Юр. Лицо не выбрано)';
+
                 $scope.getClass = function (path) {
                     if ('/' + $location.path().substr(1, path.length) == path) {
                         return "active"
@@ -22,8 +33,28 @@ angular.module('website.top.menu', [])
         return {
             restrict: 'E',
             templateUrl: 'html/partials/private/user_menu.html',
-            controller: function ($scope, $location) {
-              //TODO
+            scope: {
+                accountShowModal: '=accountShowModal',
+                companyShowModal: '=companyShowModal',
+                displayedName: '=displayedName',
+                accountName: '=accountName',
+                companyShortName: '=companyShortName'
+
+            },
+            controller: function ($scope, redirectFactory, storageFactory) {
+                $scope.showSelectAccountPopup = function () {
+                    $scope.accountShowModal = true;
+                };
+
+                $scope.showSelectCompanyPopup = function () {
+                    $scope.companyShowModal = true;
+                };
+
+                $scope.logout = function () {
+                    storageFactory.removeToken();
+                    localStorage.clear();
+                    redirectFactory.logout();
+                }
             }
         };
     })
