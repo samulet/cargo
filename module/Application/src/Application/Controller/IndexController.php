51abec4259cace602f855884834987a73a39dd2e
Oleg Lobach <oleg@lobach.info>
2013-12-16 23:52:45 +0400

    Исправил ошибку с отсутствующим пользователем

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -9,19 +9,18 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        if (!$this->zfcUserAuthentication()->hasIdentity()) {
+        $user = $this->zfcUserAuthentication()->getIdentity();
+        if (empty($user)) {
             return $this->redirect()->toUrl('/');
         }
 
-        $user = $this->zfcUserAuthentication()->getIdentity();
         $tokenEntity = $this->getServiceLocator()->get('AuthToken\Model\AuthToken')->create($user);
 
         $cookie = new SetCookie('token', $tokenEntity->getToken());
         $response = $this->getResponse()->getHeaders();
         $response->addHeader($cookie);
 
-        $viewModel = new ViewModel();
-        return $viewModel;
+        return new ViewModel();
     }
 
     public function greetingsAction()
