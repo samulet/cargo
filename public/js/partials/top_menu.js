bd3af3b0530f8b071d0353de0adb28b72a591d8d
S.Panfilov <shade241241@gmail.com>
2014-01-05 11:04:01 +0900

    Добавлена статистика по импорту пунктов доставки

--- public/js/partials/top_menu.js
+++ public/js/partials/top_menu.js
@@ -216,10 +216,10 @@ angular.module('website.top.menu', [])
         $scope.importCompaniesMessages = [];
         $scope.noCompaniesToImport = false;
         $scope.stat = {
-            newCompanies: 0,
-            changedCompanies: 0,
-            existsCompanies: 0,
-            processedCompanies: 0
+            new: 0,
+            changed: 0,
+            exists: 0,
+            processed: 0
         };
 
         function onError(data, status) {
@@ -233,10 +233,10 @@ angular.module('website.top.menu', [])
         function fetchImportStatistic() {
             for (var i = 0; i <= 0; i++) {
                 var externalService = $scope.extServiceCompanies[i];
-                $scope.stat.newCompanies = $scope.stat.newCompanies + externalService.stat.new;
-                $scope.stat.changedCompanies = $scope.stat.changedCompanies + externalService.stat.changed;
-                $scope.stat.existsCompanies = $scope.stat.existsCompanies + externalService.stat.exists;
-                $scope.stat.processedCompanies = $scope.stat.processedCompanies + externalService.stat.processed;
+                $scope.stat.new = $scope.stat.new + externalService.stat.new;
+                $scope.stat.changed = $scope.stat.changed + externalService.stat.changed;
+                $scope.stat.exists = $scope.stat.exists + externalService.stat.exists;
+                $scope.stat.processed = $scope.stat.processed + externalService.stat.processed;
             }
         }
 
@@ -256,6 +256,12 @@ angular.module('website.top.menu', [])
     .controller('importPlacesModalController', ['$scope', '$rootScope', '$http', 'REST_CONFIG', 'errorFactory', 'RESPONSE_STATUS', function ($scope, $rootScope, $http, REST_CONFIG, errorFactory, RESPONSE_STATUS) {
         $scope.importPlacesMessages = [];
         $scope.noPlacesToImport = false;
+        $scope.stat = {
+            new: 0,
+            changed: 0,
+            exists: 0,
+            processed: 0
+        };
 
         function onError(data, status) {
             if (status === RESPONSE_STATUS.NOT_FOUND) {
@@ -265,11 +271,22 @@ angular.module('website.top.menu', [])
             }
         }
 
+        function fetchImportStatistic() {
+            for (var i = 0; i <= 0; i++) {
+                var externalService = $scope.extServicePlaces[i];
+                $scope.stat.new = $scope.stat.new + externalService.stat.new;
+                $scope.stat.changed = $scope.stat.changed + externalService.stat.changed;
+                $scope.stat.exists = $scope.stat.exists + externalService.stat.exists;
+                $scope.stat.processed = $scope.stat.processed + externalService.stat.processed;
+            }
+        }
+
         $scope.importPlaces = function () {
             $http.get(REST_CONFIG.BASE_URL + '/service/import/place')
                 .success(function (data) {
                     $scope.isImportPlacesComplete = true;
-                    $scope.extServicePlaces = data._embedded.ext_service_places;
+                    $scope.extServicePlaces = data._embedded.places;
+                    fetchImportStatistic();
                 }).error(function (data, status) {
                     onError(data, status);
                 }
